7af3678595af8f6d448c7c445f0798189f7a1c32
Lee Sykes <lee.sykes@snco.us>
2023-03-16 07:30:26 -0500

    SOWA-34-KanPayController
    - fix some merge conflicts

--- SolidWaste2/SW.BLL/Services/ITransactionService.cs
+++ SolidWaste2/SW.BLL/Services/ITransactionService.cs
@@ -1,4 +1,5 @@
-﻿using SW.DM;
+﻿using SW.BLL.DTOs;
+using SW.DM;
 
 namespace SW.BLL.Services
 {
@@ -10,5 +11,20 @@ namespace SW.BLL.Services
         Task<Transaction> GetLatest(int customerId);
         Task<Transaction> GetById(int transactionId);
         Task AddKanpayTransaction(Transaction transaction, TransactionKanPayFee fee, int kanpayid, string user);
+
+        // Transaction 
+        Task<Transaction> GetLatesetTransaction(int customerId);
+
+        // Delinquency 
+        Task<decimal> GetRemainingBalanceFromLastBill(int customerId);
+        Task<decimal> GetPastDueAmount(int customerId);
+        Task<decimal> Get30DaysPastDueAmount(int customerId);
+        Task<decimal> Get60DaysPastDueAmount(int customerId);
+        Task<decimal> Get90DaysPastDueAmount(int customerId);
+        Task<decimal> GetRemainingCurrentBalance(DateTime date, int days, int customerId);
+        Task<decimal> GetCollectionsBalance(int customerId);
+        Task<decimal> GetCounselorsBalance(int customerId);
+        Task<ICollection<CustomerDelinquency>> GetAllDelinquencies();
+        Task<ICollection<Transaction>> GetPayments(DateTime thruDate, Transaction bill);
     }
 }
--- SolidWaste2/SW.BLL/Services/TransactionService.cs
+++ SolidWaste2/SW.BLL/Services/TransactionService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using SW.BLL.DTOs;
 using SW.BLL.Extensions;
 using SW.DAL.Contexts;
 using SW.DM;
@@ -7,22 +8,22 @@ namespace SW.BLL.Services;
 
 public class TransactionService : ITransactionService
 {
-    private readonly IDbContextFactory<SwDbContext> contextFactory;
+    private readonly IDbContextFactory<SwDbContext> dbFactory;
 
     public TransactionService(IDbContextFactory<SwDbContext> contextFactory)
     {
-        this.contextFactory = contextFactory;
+        this.dbFactory = contextFactory;
     }
 
     internal async Task<bool> TransactionExists(int transactionId)
     {
-        using var db = contextFactory.CreateDbContext();
+        using var db = dbFactory.CreateDbContext();
         return await db.Transactions.AnyAsync(e => e.Id == transactionId);
     }
 
     public async Task AddTransaction(Transaction transaction)
     {
-        using var db = contextFactory.CreateDbContext();
+        using var db = dbFactory.CreateDbContext();
         var lastTransaction = await db.Transactions
             .Where(e => e.CustomerId == transaction.CustomerId && !e.DeleteFlag)
             .OrderByDescending(t => t.AddDateTime)
@@ -40,7 +41,7 @@ public class TransactionService : ITransactionService
 
     public async Task<ICollection<Transaction>> GetByCustomer(int customerId)
     {
-        using var db = contextFactory.CreateDbContext();
+        using var db = dbFactory.CreateDbContext();
         return await db.Transactions
             .Where(e => e.CustomerId == customerId && !e.DeleteFlag)
             .Include(e => e.Customer)
@@ -51,7 +52,7 @@ public class TransactionService : ITransactionService
 
     public async Task<Transaction> GetLatest(int customerId)
     {
-        using var db = contextFactory.CreateDbContext();
+        using var db = dbFactory.CreateDbContext();
         return await db.Transactions
             .Where(e => e.CustomerId == customerId && !e.DeleteFlag)
             .OrderByDescending(e => e.AddDateTime)
@@ -68,7 +69,7 @@ public class TransactionService : ITransactionService
 
     public async Task<Transaction> GetById(int transactionId)
     {
-        using var db = contextFactory.CreateDbContext();
+        using var db = dbFactory.CreateDbContext();
         return await db.Transactions
             .Where(e => e.Id == transactionId)
             .AsNoTracking()
@@ -77,7 +78,7 @@ public class TransactionService : ITransactionService
 
     public async Task AddKanpayTransaction(Transaction transaction, TransactionKanPayFee fee, int kanpayid, string user)
     {
-        using var db = contextFactory.CreateDbContext();
+        using var db = dbFactory.CreateDbContext();
 
         //START the following code was copied from #region Transaction (public void AddTransactionAutoCalc(Transaction transaction, string user)) to faciliate a DATA BASE transaction scope encompassing three tables.                                   
         transaction.AddToi = user;
@@ -273,4 +274,197 @@ public class TransactionService : ITransactionService
 
         await db.SaveChangesAsync();
     }
+
+    #region Utility 
+
+    internal async Task<Customer> GetCustomerById(int id)
+    {
+        using var db = dbFactory.CreateDbContext();
+        return await db.Customers
+            .Where(c => c.CustomerId == id || c.LegacyCustomerId == id)
+            .AsNoTracking()
+            .SingleAsync();
+    }
+
+    internal async Task<ICollection<Customer>> GetAllCustomers()
+    {
+        using var db = dbFactory.CreateDbContext();
+        return await db.Customers
+            .AsNoTracking()
+            .ToListAsync();
+    }
+
+    #endregion
+
+    #region Transaction 
+
+    public async Task<Transaction> GetLatesetTransaction(int customerId)
+    {
+        using var db = dbFactory.CreateDbContext();
+
+        var customer = await GetCustomerById(customerId);
+
+        return await db.Transactions
+            .Where(t => t.CustomerId == customer.CustomerId && !t.DeleteFlag)
+            .OrderByDescending(t => t.AddDateTime).ThenByDescending(t => t.Sequence).ThenByDescending(t => t.Id)
+            .AsNoTracking()
+            .FirstOrDefaultAsync();
+    }
+
+    #endregion
+
+    #region Delinquency 
+
+    public async Task<decimal> GetRemainingBalanceFromLastBill(int customerId)
+    {
+        return await GetRemainingCurrentBalance(0, customerId);
+    }
+
+    internal async Task<decimal> GetPastDueAmount(int daysPastDue, int customerId)
+    {
+        return await GetRemainingCurrentBalance(daysPastDue + 14, customerId);
+    }
+
+    public async Task<decimal> GetPastDueAmount(int customerId)
+    {
+        return await GetPastDueAmount(1, customerId);
+    }
+
+    public async Task<decimal> Get30DaysPastDueAmount(int customerId)
+    {
+
+        return await GetPastDueAmount(30, customerId);
+    }
+
+    public async Task<decimal> Get60DaysPastDueAmount(int customerId)
+    {
+
+        return await GetPastDueAmount(60, customerId);
+    }
+
+    public async Task<decimal> Get90DaysPastDueAmount(int customerId)
+    {
+
+        return await GetPastDueAmount(90, customerId);
+    }
+
+    internal async Task<decimal> GetRemainingCurrentBalance(int days, int customerId)
+    {
+        return await GetRemainingCurrentBalance(DateTime.Now, days, customerId);
+    }
+
+    public async Task<decimal> GetRemainingCurrentBalance(DateTime date, int days, int customerId)
+    {
+        using var db = dbFactory.CreateDbContext();
+
+        var endDate = date.Date.AddDays(1);
+        var startDate = date.Date.AddDays(-days);
+        Customer customer = await GetCustomerById(customerId);
+        string[] billTypes = { "MB", "FB", "MBR" };
+
+        var bill = await db.Transactions
+            .Where(t => t.CustomerId == customer.CustomerId
+                && !t.DeleteFlag
+                && billTypes.Contains(t.TransactionCode.Code)
+                && t.AddDateTime < startDate)
+            .OrderByDescending(t => t.AddDateTime).ThenByDescending(t => t.Sequence).ThenByDescending(t => t.Id)
+            .AsNoTracking()
+            .FirstOrDefaultAsync();
+
+        if (bill == null || bill.TransactionBalance <= 0)
+            return 0;
+
+        var transactions = await db.Transactions
+            .Where(t => t.CustomerId == customer.CustomerId
+                && !t.DeleteFlag
+                && t.AddDateTime < endDate
+                && t.TransactionAmt < 0
+                && ((t.AddDateTime > bill.AddDateTime) || (t.AddDateTime == bill.AddDateTime && t.Sequence > bill.Sequence)))
+            .AsNoTracking()
+            .ToListAsync();
+
+        var total = bill.TransactionBalance + transactions.Sum(t => t.TransactionAmt);
+        if (total < 0)
+            return 0;
+
+        return total;
+    }
+
+    public async Task<decimal> GetCollectionsBalance(int customerId)
+    {
+        var t = await GetLatesetTransaction(customerId);
+        return t != null ? t.CollectionsBalance : 0;
+    }
+
+    public async Task<decimal> GetCounselorsBalance(int customerId)
+    {
+        var t = await GetLatesetTransaction(customerId);
+        return t != null ? t.CounselorsBalance : 0;
+    }
+
+    public async Task<ICollection<CustomerDelinquency>> GetAllDelinquencies()
+    {
+        var list = new List<CustomerDelinquency>();
+        var customers = await GetAllCustomers();
+
+        foreach (var c in customers)
+        {
+            var d = new CustomerDelinquency();
+            d.Customer = c;
+            d.CollectionsBalance = await GetCollectionsBalance(c.CustomerId);
+            d.CounselorsBalance = await GetCounselorsBalance(c.CustomerId);
+
+            var d1 = await GetPastDueAmount(c.CustomerId);
+
+            if (d1 > 0)
+            {
+                var d30 = await Get30DaysPastDueAmount(c.CustomerId);
+                var d60 = await Get60DaysPastDueAmount(c.CustomerId);
+                var d90 = await Get90DaysPastDueAmount(c.CustomerId);
+
+                d.PastDue = d1 - d30;
+                if (d.PastDue < 0)
+                    d.PastDue = 0;
+
+                d.PastDue30Days = d30 - d60;
+                if (d.PastDue30Days < 0)
+                    d.PastDue30Days = 0;
+
+                d.PastDue60Days = d60 - d90;
+                if (d.PastDue60Days < 0)
+                    d.PastDue60Days = 0;
+
+                d.PastDue90Days = d90;
+                if (d.PastDue90Days < 0)
+                    d.PastDue90Days = 0;
+            }
+
+            // check & service call should be moved to controller where this method is being called from? 
+            //var pebl = new PE.BL.BusinessLayer(); 
+            //if (d.IsDelinquent) 
+            //{ 
+            //    d.PersonEntity = pebl.GetPersonEntityById(c.PE); 
+            //    list.Add(d); 
+            //} 
+        }
+
+        return list;
+    }
+
+    public async Task<ICollection<Transaction>> GetPayments(DateTime thruDate, Transaction bill)
+    {
+        using var db = dbFactory.CreateDbContext();
+
+        var date = thruDate.Date.AddDays(1);
+        return await db.Transactions
+            .Where(t => !t.DeleteFlag
+                && t.TransactionAmt < 0
+                && t.CustomerId == bill.CustomerId
+                && t.AddDateTime < date
+                && ((t.AddDateTime > bill.AddDateTime) || (t.AddDateTime == bill.AddDateTime && t.Sequence > bill.Sequence)))
+            .AsNoTracking()
+            .ToListAsync();
+    }
+
+    #endregion
 }
