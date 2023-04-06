d055ca38dcd93975541ed7b9b7a8e661d2152e13
Lee Sykes <lee.sykes@snco.us>
2023-04-06 08:23:17 -0500

    SOWA-70-Internal-Container-Rate-controller
    - added container rate controller, views, and logic

--- SolidWaste2/SW.BLL/Services/IContainerSubtypeService.cs
+++ SolidWaste2/SW.BLL/Services/IContainerSubtypeService.cs
@@ -4,6 +4,8 @@ namespace SW.BLL.Services
 {
     public interface IContainerSubtypeService
     {
+        Task<ICollection<ContainerSubtype>> GetAll();
+        Task<ICollection<ContainerSubtype>> GetByContainerType(int containerCodeId);
         Task<ContainerSubtype> GetById(int containerSubtypeId);
     }
 }
