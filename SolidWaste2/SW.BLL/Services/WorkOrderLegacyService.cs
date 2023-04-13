9335bf2751a38d362e15a82529255b4ee2cfcbbb
Neil Briggs <neil.briggs@snco.us>
2023-04-13 09:32:07 -0500

    SOWA-98   Internal - Work Order Legacy Inquiry controller
    - Converted the business logic for the legacy work order inquiry from raw sql to a linq based implementation
    - Converted the controller, model, and views
    - Moved the class definition for the inquiry results into the view model and mapped those values at the controller

--- SolidWaste2/SW.BLL/Services/WorkOrderLegacyService.cs
+++ SolidWaste2/SW.BLL/Services/WorkOrderLegacyService.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using SW.BLL.DTOs;
 using SW.DAL.Contexts;
 using SW.DM;
 
@@ -29,5 +31,42 @@ public class WorkOrderLegacyService : IWorkOrderLegacyService
         using var db = dbFactory.CreateDbContext();
         db.WorkOrderLegacies.Update(workOrderLegacy);
         await db.SaveChangesAsync();
+    }
+
+    public async Task<ICollection<WorkOrderLegacy>> GetInquiryResultList(int? equipmentNumber, string route, DateTime? transDate, string driver, string breakdownLocation, int? problemNumber, bool include)
+    {
+        using var db = dbFactory.CreateDbContext();
+
+         IQueryable<WorkOrderLegacy> query = db.WorkOrderLegacies
+            .Where(e => !e.DelFlag)
+            .AsNoTracking();
+
+        if (equipmentNumber.HasValue)
+            query = query.Where(e => e.EquipmentNumber == equipmentNumber);
+
+        if (!string.IsNullOrWhiteSpace(route))
+            query = query.Where(e => e.EquipmentNumber == equipmentNumber);
+
+        if (transDate.HasValue)
+            query = query.Where(e => e.TransDate == transDate);
+
+        if (!string.IsNullOrWhiteSpace(driver))
+            query = query.Where(e => e.Driver == driver);
+
+        if (!string.IsNullOrWhiteSpace(breakdownLocation))
+            query = query.Where(e => e.BreakdownLocation.Contains(breakdownLocation));
+
+        if (problemNumber.HasValue)
+            query = query.Where(e => e.ProblemNumber == problemNumber);
+
+        //if (include)        //this is checked so show closed work orders   (yes   resolved date)
+        //    query = query.Where(e => e.ResolveDate.HasValue);
+
+        //if (!include)       //this is unchecked so show open work orders   (no   resolved date)
+        //    query = query.Where(e => !e.ResolveDate.HasValue);
+
+        return await query
+            .Take(500)
+            .ToListAsync();
     }
 }
