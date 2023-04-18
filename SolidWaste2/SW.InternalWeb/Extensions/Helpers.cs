c079fda5c024cdd11a964cd798fde2ac9e06777f
Lee Sykes <lee.sykes@snco.us>
2023-04-18 14:05:07 -0500

    SOWA-75-Internal-Customer-Phone-Number-controller
    - added customer phone controller

--- SolidWaste2/SW.InternalWeb/Extensions/Helpers.cs
+++ SolidWaste2/SW.InternalWeb/Extensions/Helpers.cs
@@ -4,47 +4,49 @@ namespace SW.InternalWeb.Extensions;
 
 public static class Helpers
 {
-    public static List<SelectListItem> CustomerCodes
-    {
-        get
+    public static IEnumerable<SelectListItem> CustomerCodes =>
+        new SelectListItem[]
         {
-            return new List<SelectListItem>()
-            {
-                new SelectListItem { Value = "C", Text = "C - Commercial" },
-                new SelectListItem { Value = "R", Text = "R - Residential" },
-                new SelectListItem { Value = "H", Text = "H - Home Owners Association" }
-            };
-        }
-    }
+            new SelectListItem { Value = "C", Text = "C - Commercial" },
+            new SelectListItem { Value = "R", Text = "R - Residential" },
+            new SelectListItem { Value = "H", Text = "H - Home Owners Association" }
+        };
 
-    public static List<SelectListItem> EmailTypes
-    {
-        get
+    public static IEnumerable<SelectListItem> EmailTypes =>
+        new SelectListItem[]
         {
-            return new List<SelectListItem>()
-            {
-                new SelectListItem { Value = "7", Text = "P - Personal" },
-                new SelectListItem { Value = "8", Text = "W - Work" }
-            };
-        }
-    }
+            new SelectListItem { Value = "7", Text = "P - Personal" },
+            new SelectListItem { Value = "8", Text = "W - Work" }
+        };
 
-    public static List<SelectListItem> EmailStatuses
-    {
-        get
+    public static IEnumerable<SelectListItem> EmailStatuses =>
+        new SelectListItem[]
         {
-            return new List<SelectListItem>()
-            {
-                new SelectListItem { Value = "true", Text = "Active" },
-                new SelectListItem { Value = "false", Text = "Inactive" }
-            };
-        }
-    }
+            new SelectListItem { Value = "true", Text = "Active" },
+            new SelectListItem { Value = "false", Text = "Inactive" }
+        };
 
-    public static IEnumerable<SelectListItem> DeliveryOptions => new List<SelectListItem>()
-    {
-        new SelectListItem { Value = "1", Text = "Email only" },
-        new SelectListItem { Value = "2", Text = "Paper only" },
-        new SelectListItem { Value = "3", Text = "Both Email and Paper" }
-    };
+    public static IEnumerable<SelectListItem> DeliveryOptions =>
+        new SelectListItem[]
+        {
+            new SelectListItem { Value = "1", Text = "Email only" },
+            new SelectListItem { Value = "2", Text = "Paper only" },
+            new SelectListItem { Value = "3", Text = "Both Email and Paper" }
+        };
+
+    public static IEnumerable<SelectListItem> PhoneTypes =>
+        new SelectListItem[]
+        {
+            new SelectListItem { Value = "3", Text = "H - Home"},
+            new SelectListItem { Value = "4", Text = "W - Work"},
+            new SelectListItem { Value = "5", Text = "C - Cell"},
+            new SelectListItem { Value = "6", Text = "F - Fax"}
+        };
+
+    public static IEnumerable<SelectListItem> PhoneStatuses =>
+        new SelectListItem[]
+        {
+            new SelectListItem { Value = "true", Text = "Active" },
+            new SelectListItem { Value = "false", Text = "Inactive" }
+        };
 }
